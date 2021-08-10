dd7d4cab4a649543d79a38eebe65263a559b7376
Kevin Mathew <kevinam99@gmail.com>
2021-08-10 21:46:16 +0530

    First request successful

--- lib/gpt3_test.ex
+++ lib/gpt3_test.ex
@@ -1,4 +1,6 @@
-defmodule Gpt3Test do
+defmodule GPT3Test do
+  require HTTPoison
+  # import Jason
   @moduledoc """
   Module to test with GPT 3
   """
@@ -8,13 +10,35 @@ defmodule Gpt3Test do
 
   ## Examples
 
-      iex> Gpt3Test.hello()
-      :world
+  iex> Gpt3Test.hello()
+  :world
 
   """
 
-
-  defp api_key do
+  defp api_key() do
     ""
+  end
+
+  defp data() do
+    """
+    {"prompt": "This is a test", "max_tokens": 5}
+    """
+  end
+
+  defp headers() do
+    [
+      "Content-Type": "application/json",
+      Authorization: "Bearer #{api_key()}"
+    ]
+  end
+
+  defp url() do
+    "https://api.openai.com/v1/engines/davinci/completions"
+  end
+
+  def start() do
+    HTTPoison.start()
+    {:ok, response} = HTTPoison.post(url(), data(), headers())
+    IO.inspect(response.body)
   end
 end
