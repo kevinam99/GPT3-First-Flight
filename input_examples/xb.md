479e3bad5d7de6a53bdd508bbf5011927ce336d9
Kevin Mathew <kevinam99@gmail.com>
2021-08-12 08:11:31 +0530

    Create poll answer classifier
    This classifier takes in examples of valid and invalid poll answers and then
    learns from it.
    
    Subsequently, pass in an input string and then it will return the classification ,i.e,
    whether the string is valid or invalid.

---
+++ input_examples/xb.md
@@ -0,0 +1,30 @@
+0. i think this option is more clear
+1.  option b font style is better in my opinion
+2. More minimalistic
+3. Looks more modern
+4. that was original
+5. i like optr
+6. Option B seemed like the much cleaner choice of the two. It is easy to notice, read and understand.
+7. Option B has a simple and clean design.
+8. They both look exactly the same. I believe both are suitable!!
+9. The second logo B is clear. so i like the send logo.
+10. Seems more professional and cleaner.
+11. The font just seems more neat and clean.
+12. good design
+13. option 2 is better than 1
+14. neat design
+15. It is simple and attracts in quick time.
+16. I think the text font looks cleaner with option B. It's also easier to read from a greater distance.
+17. Option A looks dated and somewhat threatening.
+18. Very clear and neat add.  Looks good.
+19. More Appealing
+20. because of clarity
+21. I think the font is better in Option B it seems more uniform and modern.
+22. It is easier to read.
+23. The second one matches more with the logo. I just don't like the details on the image A
+24. The font used in Option B is cleaner.
+25. Option B's text at the bottom is far more legible.  The font used in Option A is fancier but makes the text seem blurrier and much harder to read.  Overall Option B seems much sharper.
+26. It looks good and decent for looking. 
+27. I like the flatter look of the font, it looks more modern. 
+28. B is the best  online Sports/Gaming company.
+29. the font is cleaner and easier to read.
---
+++ lib/gpt3_test/classification-poll.ex
@@ -0,0 +1,112 @@
+defmodule GPT3Test.ClassificationPoll do
+  import Secrets
+
+  @moduledoc """
+  Implementing text classification using the Ada engine.
+  The example shown here is what we used in t=our final year
+  BE project.
+
+  It understood the training examples well and it handles spelling mistakes.
+  It also handles grmmatical errors.
+  """
+
+  # defp api_key() do
+  #   "<PASTE_YOUR_API_KEY_HERE>"
+  # end
+
+  defp data(query) do
+    request_body = %{
+      # A list of examples with labels, in the follwing format:
+      examples: training_examples(),
+      # Query to be classified.
+      query: query,
+      # ID of the engine to use for Search. Deafault to Ada.
+      search_model: "ada",
+      model: "curie",
+      labels: ["valid", "invalid"]
+    }
+
+    {:ok, json} = Jason.encode(request_body)
+    # Jason will encode the map into a compatible
+    # json form
+    json
+  end
+
+  # can either write the examples manually in the code or put them
+  # in a file and then read that file.
+
+  defp headers() do
+    [
+      "Content-Type": "application/json",
+      Authorization: "Bearer #{api_key()}"
+    ]
+  end
+
+  defp parse(body) do
+    line_matcher = ~r{(\r|\n|\r\n)}
+    clean_matcher = ~r/\d+\.\s*[a-zA-z]/
+    sno_matcher = ~r/\d+\.\s*/
+
+    body
+    |> String.split(line_matcher)
+    |> Enum.filter(&(Regex.match?(clean_matcher, &1) == true))
+    |> Enum.map(&Regex.replace(sno_matcher, &1, ""))
+  end
+
+  defp training_examples do
+    # copy and paste (return) the output of the following
+    # read_training_data()
+
+    {:ok, data} = File.read("input_examples/poll.json")
+    {:ok, examples} = Jason.decode(data)
+    examples
+  end
+
+  defp url() do
+    "https://api.openai.com/v1/classifications"
+  end
+
+  defp read_training_data do
+    filename = "xb.md"
+
+    case File.read(filename) do
+      {:ok, body} ->
+        body |> parse
+
+      {:error, reason} ->
+        IO.puts("Error: #{:file.format_error(reason)} for file #{filename}")
+        {:error, reason}
+
+      _ ->
+        IO.puts("Enter a valid filename")
+        {:error}
+    end
+  end
+
+  def start(query) do
+    HTTPoison.start()
+
+    case HTTPoison.post(url(), data(query), headers()) do
+      # if success, output the result
+      {:ok, %HTTPoison.Response{status_code: 200, body: response}} ->
+        {:ok, body} = Jason.decode(response)
+        {:ok, query, body["label"]}
+
+      # if failure, inspect error
+      {:ok, %HTTPoison.Response{status_code: 400, body: response}} ->
+        {:ok, error_msg} = Jason.decode(response)
+        {:error, error_msg}
+
+      # if url not found
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        {:error, "The URL #{url()} does not exist"}
+
+      # Invalid auth header.
+      {:ok, %HTTPoison.Response{status_code: 401, body: response}} ->
+        {:error, "Something bad happened, #{response}"}
+
+      {:error, %HTTPoison.Error{id: nil, reason: :timeout}} ->
+        {:error, "The request timed out"}
+    end
+  end
+end
